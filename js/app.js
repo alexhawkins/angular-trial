d779bbd8231465875c42d7cc3bb7340ac6c6a0e1
Alex Hawkins <alexhawkins.me@gmail.com>
2014-10-14 19:30:46 -0700

    Add images to products and Angular filters

--- js/app.js
+++ js/app.js
@@ -1,18 +1,46 @@
-
 //Wrap entire application in a closure. This is good practice in JavaScript.
-(function(){
-  var app = angular.module('store', []);
+(function() {
+    var app = angular.module('store', []);
 
-  //controller
-  app.controller('StoreController', function(){
-    this.product = gem;
+    //controller
+    app.controller('StoreController', function() {
+        this.products = gems;
 
-  });
+    });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'A really awesome bar of soap!'
-  }
+    var gems = [
+
+        {
+            name: 'Dodecahedron',
+            price: 2.00,
+            description: 'A really awesome bar of soap!',
+            canPurchase: true,
+            soldOut: false,
+            images: [{
+                full: 'img/images-2.jpeg',
+                thumb: 'img/images-2.jpeg'
+            }]
+        }, {
+            name: 'Pentagon',
+            price: 5.95,
+            description: 'A really cool slide!',
+            canPurchase: true,
+            soldOut: false,
+            images: [{
+                full: 'img/images-3.jpeg',
+                thumb: 'img/images-2.jpeg'
+            }]
+        }, {
+            name: 'Retarded Dolls',
+            price: 22.95,
+            description: 'A really retarded looking doll!',
+            canPurchase: true,
+            soldOut: false,
+            images: [{
+                full: 'img/images-2.jpeg',
+                thumb: 'img/images-2.jpeg'
+            }]
+        }
+    ];
 
 })();
