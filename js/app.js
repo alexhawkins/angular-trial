7df318b42186dd796b37d44683ef53d15d72000c
Alex Hawkins <alexhawkins.me@gmail.com>
2014-10-16 13:33:59 -0700

    Add angular serverices for parsing JSON data from server

--- js/app.js
+++ js/app.js
@@ -9,9 +9,16 @@
      */
 
     /** STORE CONTROLLER */
-    app.controller('StoreController', function() {
-        this.products = stuff;
-    });
+    app.controller('StoreController', ['$http', function($http) {
+        /** save value of this in var to use in success callback below */
+        var store = this;
+        /** initialize to an empty array so there are no erros on page load */
+        store.products = [];
+        /** get json product date from server **/
+        $http.get('store-products.json').success(function(data) {
+            store.products = data;
+        });
+    }]);
 
     /** REVIEW CONTROLLER */
     app.controller('ReviewController', function() {
@@ -27,77 +34,6 @@
     /**
      * DIRECTIVES (dependent on store-products)
      */
-    
     /** see products.js */
-
-    /**
-     * MOCK DATABASE
-     */
-
-    var stuff = [
-
-        {
-            name: 'Nike Air Swoosh',
-            price: 110.50,
-            description: 'A really awesome bar of soap!',
-            canPurchase: true,
-            soldOut: false,
-            shine: 8,
-            rarity: 7,
-            color: '#CCC',
-            faces: 14,
-            images: [{
-                full: 'img/images-2.jpeg',
-                thumb: 'img/images-2.jpeg'
-            }],
-            reviews: [{
-                stars: 5,
-                body: 'I love this product!',
-                author: 'joe@thomas.com'
-            }]
-        }, {
-            name: 'Bilbo Baggins Dunk',
-            price: 55.95,
-            description: 'A really cool slide!',
-            canPurchase: true,
-            soldOut: false,
-            shine: 9,
-            rarity: 6,
-            color: '#EEE',
-            faces: 12,
-            images: [{
-                full: 'img/images-3.jpeg',
-                thumb: 'img/images-2.jpeg'
-            }],
-            reviews: [{
-                stars: 3,
-                body: 'I love this product!',
-                author: 'alex@thomas.com'
-            }]
-        }, {
-            name: 'Retarded Doll Shoes',
-            price: 72.95,
-            description: 'A really retarded looking doll!',
-            canPurchase: true,
-            soldOut: false,
-            shine: 70,
-            rarity: 2,
-            color: '#000',
-            faces: 6,
-            images: [{
-                full: 'img/images-2.jpeg',
-                thumb: 'img/images-2.jpeg'
-            }],
-            reviews: [{
-                stars: 4,
-                body: 'I love this product!',
-                author: 'doug@thomas.com'
-            }, {
-                stars: 1,
-                body: 'I hate this product!',
-                author: 'hauser@doogie.com'
-            }]
-        }
-    ];
-
+    
 })();
